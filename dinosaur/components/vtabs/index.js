de23ec3ff4d14f68de7bd5e3b5da5323c78b297a
沈青松 <jerry@shenqingsongdeMacBook-Pro.local>
2023-06-08 02:09:47 +0800

    20230607 MAC

--- dinosaur/components/vtabs/index.js
+++ dinosaur/components/vtabs/index.js
@@ -33,17 +33,85 @@ Component({
   },
   relations: {
     "../vtabs-content/index": {
-      type: "child"
+      type: "child",
+      linked: function(target){
+        let _this = this
+        target.calcHeight(function (rect) {
+          _this.data._contentHeight[target.data.tabIndex] = rect.height;
+          if (_this._calcHeightTimer) {
+              clearTimeout(_this._calcHeightTimer);
+          }
+          _this._calcHeightTimer = setTimeout(function () {
+              _this.calcHeight();
+          }, 100);
+      });
+      },
+      unlinked: function unlinked(target) {
+        delete this.data._contentHeight[target.data.tabIndex];
+    }
     } 
+  },
+  observers: {
+    activeTab: function(_activeTab){
+      this.scrollTabBar(_activeTab)
+    }
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    // 计算右侧item分类框高度
+    calcHeight: function(){
+      let length = this.data.vtabs.length;
+      let _contentHeight = this.data._contentHeight;
+      let _heightRecords = [];
+      let temp = 0;
+      for(let i = 0; i < length; i++){
+        _heightRecords[i] = temp + (_contentHeight[i] || 0);
+        temp = _heightRecords[i]
+      }
+      this.data._heightRecords = _heightRecords;
+    },
     // 左侧导航栏分类点击函数
-    handleTabClick: function(){
-      
+    handleTabClick: function(e){
+      var _heightRecords = this.data._heightRecords;
+      var index = e.currentTarget.dataset.index;
+      var contentScrollTop = _heightRecords[index - 1] || 0;
+      this.triggerEvent('tabclick', { index: index });
+      this.setData({
+          activeTab: index,
+          contentScrollTop: contentScrollTop
+      });
+    },
+
+    scrollTabBar: function(index){
+      let len = this.data.vtabs.length;
+      if (len === 0) return;
+      let currentView = index < 6 ? 0 : index - 5;
+      if (currentView >= len) currentView = len - 1;
+      this.setData({
+        currentView: currentView
+      })
+    },
+    handleContentScroll: function handleContentScroll(e) {
+      var _heightRecords = this.data._heightRecords;
+      if (_heightRecords.length === 0) return;
+      var length = this.data.vtabs.length;
+      var scrollTop = e.detail.scrollTop;
+      var index = 0;
+      if (scrollTop >= _heightRecords[0]) {
+          for (var i = 1; i < length; i++) {
+              if (scrollTop >= _heightRecords[i - 1] && scrollTop < _heightRecords[i]) {
+                  index = i;
+                  break;
+              }
+          }
+      }
+      if (index !== this.data.activeTab) {
+          this.triggerEvent('change', { index: index });
+          this.setData({ activeTab: index });
+      }
     }
   }
 })
